6efe183c0216f2e1d4e7bb7c69cc12b62fc75891
khaoula <khawoulaelhonnya7ma@gmail.com>
2026-05-09 18:35:03 +0100

    Fix settings et CI pipeline

--- medibook/settings.py
+++ medibook/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'medibook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE':  'django.db.backends.sqlite3',
         'NAME':BASE_DIR / 'db.sqlite3',
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
